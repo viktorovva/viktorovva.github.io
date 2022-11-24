90a7dfef634154a84aab535636740c472aea31b1
viktorovva <118428199+viktorovva@users.noreply.github.com>
2022-11-25 02:16:15 +0600

    Add files via upload

--- index/script.js
+++ index/script.js
@@ -1,5 +1,5 @@
 // Started Logo
 window.onload = ev => {
-    document.querySelector('.wrapper').style.opacity = 1;
-    setTimeout(() => document.querySelector('.preload').classList.add('invisible'), 500)
+	document.querySelector('.wrapper').style.opacity = 1;
+	setTimeout(() => document.querySelector('.preload').classList.add('invisible'), 500)
 }
